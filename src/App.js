73f84b82a9d379a97473f5deb398553209411911
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-21 11:32:07 -0700

    add login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 import './App.css';
 import Navbar from './Component/Header/Navbar';
+import LoginForm from './Component/LoginForm/LoginForm';
 
 function App() {
   return (
     <Fragment>
       <Navbar />
+      <LoginForm />
     </Fragment>
   );
 }
