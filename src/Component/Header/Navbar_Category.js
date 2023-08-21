38602af76bc50399461ec6f4dd7955ee52a01db8
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-21 09:17:40 -0700

    all website category

---
+++ src/Component/Header/Navbar_Category.js
@@ -0,0 +1,42 @@
+import classes from "./Navbar_Category.module.css";
+
+const navbarCategory = () => {
+    return (
+        <div className={classes.category}>
+            <details >
+                <summary>Sports</summary>
+                <div className={classes.links}>
+                    <a href=" ">Cricket</a>
+                    <a href=" ">Soocer</a>
+                    <a href=" ">Hookey</a>
+                </div>
+            </details>
+            <details >
+                <summary>Health</summary>
+                <div className={classes.links}>
+                    <a href=" ">ChildHealth</a>
+                    <a href=" ">Man_Health</a>
+                    <a href=" ">Woman_Health</a>
+                </div>
+            </details>
+            <details >
+                <summary>Media</summary>
+                <div className={classes.links}>
+                    <a href=" ">National_Media</a>
+                    <a href=" ">InterNational <br/>Media</a>
+                </div>
+            </details>
+            <details >
+                <summary>Fitness</summary>
+                <div className={classes.links}>
+                    <a href=" ">GYM_Fitness</a>
+                    <a href=" ">Sports_Fitness</a>
+                    <a href=" ">Regular_Fitness</a>
+                </div>
+            </details>
+            
+
+        </div>
+    )
+}
+export default navbarCategory;
