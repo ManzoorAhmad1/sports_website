d13f3c58410c8dbb85bb3def0f01c6f5b59625fb
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-24 04:14:46 -0700

    add cricket news backened data

---
+++ src/Backened_Folder/Sports/CricketNews.js
@@ -0,0 +1,69 @@
+const CricketNews = [
+    {
+        "id": "a1",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339300/laura-takes-over-from-sune-luu.jpg",
+        "title": "Laura Wolvaardt named interim South Africa Women captain",
+        "description": "She will lead the teams for the upcoming series against Pakistan and New Zealand.",
+    },
+    {
+        "id": "a2",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c269604/ebadot-missed-out-on-asia-cup.jpg",
+        "title": "Ebadot Hossain in race against time for World Cup ",
+        "description": "BCB official confirmed to Cricbuzz that they are contemplating sending Ebadot abroad for a second opinion on his knee injury",
+    },
+    {
+        "id": "a3",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339139/this-edition-efforts-have-bee.jpg",
+        "title": "Season 2 of ILT20 kicks off on January 19",
+        "description": "This edition, efforts have been made to minimize the overlap with SA20",
+    },
+    {
+        "id": "a4",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339114/bell-is-set-to-assist-new-zeal.jpg",
+        "title": "Bell, Fleming and Saqlain to join NZ coaching group on away tours",
+        "description": "New Zealand want to offer breaks to their permanent coaches as well as introduce fresh ideas to coaching group",
+    },
+    {
+        "id": "a5",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339113/the-changes-will-be-implemente.jpg",
+        "title": "Equal match fees, professional league for South Africa's women",
+        "description": "Not only will women's international players earn the same match fees as their male counterparts, the women's domestic leagues will be professiona",
+    },
+    {
+        "id": "a6",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c333768/zaka-ashrafs-term-could-come.jpg",
+        "title": "Zaka Ashraf may be on way out; Najam Sethi may return",
+        "description": "The political changes in the country are set to pave the way for another churn in the PCB leadership",
+    },
+    {
+        "id": "a7",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339092/spencer-johnson-has-had-a-rapi.jpg",
+        "title": "Mitchell Marsh backs Spencer Johnson to succeed in international cricket ",
+        "description": "The speedster is expected to make his debut for Australia in limited-overs series in South Africa",
+    },
+    {
+        "id": "a8",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339079/rohit-wants-the-middle-order-t.jpg",
+        "title": "Rohit emphasises importance of batting position flexibility",
+        "description": "Indian captain said players have been told to not get fixated with batting positions",
+    },
+    {
+        "id": "a9",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339254/how-much-of-haris-rauf-will-be.jpg",
+        "title": "The Asia Cup Pulse",
+        "description": "Byte-sized briefings on all things Asia Cup 2023",
+    },
+    {
+        "id": "a10",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339254/how-much-of-haris-rauf-will-be.jpg",
+        "title": "BCCI says no to HCA request, World Cup schedule remains unaltered",
+        "description": "The venue is set to host matches on back-to-back days",
+    },
+    {
+        "id": "a11",
+        "imgToUrl": "https://www.cricbuzz.com/a/img/v1/205x152/i1/c339064/one-batting-slot-does-not-make.jpg",
+        "title": "India don't have any No.4 problem, reckons Ganguly",
+        "description": "The former India captain voiced out in support of Yuzvendra Chahal's selection although the legspinner was subsequently left out of the Asia Cup squad",
+    },
+]
+export default CricketNews;
