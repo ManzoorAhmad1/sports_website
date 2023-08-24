7d68abc83aeb3396a0b953fc1c863f79459e3826
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-24 08:47:36 -0700

    add leages team

---
+++ src/Component/Sports/Country_League/Leagues_Teams/Leagues_TeamsDetail.js
@@ -0,0 +1,16 @@
+const TeamDetails = () => {
+    return (
+        <div>
+            <div><h1>Leagues List</h1></div>
+            <div>
+                <ul>
+                    <li><a href="PSL">PSLs</a></li>
+                    <li><a href="IPL">IPL</a></li>
+                    <li><a href="BPL">BPL</a></li>
+                    <li><a href="LPL">LPL</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default TeamDetails;
