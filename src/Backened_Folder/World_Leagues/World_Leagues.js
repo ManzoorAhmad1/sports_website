2a16275caa2c071c2ce1395b0e4c2bcdeeb4b047
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-24 07:16:55 -0700

    world league backend data

---
+++ src/Backened_Folder/World_Leagues/World_Leagues.js
@@ -0,0 +1,27 @@
+const WorldLeagues = [
+    {
+        "id": "a1",
+        "imageToUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcexMnVuxDwqHg7dcH2FqG6qgPy7b-AwuH0A&usqp=CAU",
+        "teamName": "Pakistan Premium League (PSL)",
+        "description": "The Pakistan Super League is a men's Twenty20 cricket league contested by six city-based franchise teams. The league was founded in 2015 by the Pakistan Cricket Board."
+    },
+    {
+        "id": "a2",
+        "imageToUrl": "https://www.mykhel.com/common_dynamic/images/leagues/ipl-bannerimg.png",
+        "teamName": "Indian Premium League (IPL)",
+        "description": "The sixteenth edition of the Indian Premier League started on March 31 at the Narendra Modi Stadium in Ahmedabad with defending champions Gujarat Titans facing eventual winners Chennai Super Kings in the tournament opener. 10 teams battled for 60 days with an aim to lift the coveted trophy."
+    },
+    {
+        "id": "a4",
+        "imageToUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu0-ZNkubDIrsMWPp693_7t-oP-zJQxzP0Dw&usqp=CAU",
+        "teamName": "Bangladesh Premium League (BPL)",
+        "description": "The Bangladesh Premier League is a professional Twenty20 cricket league organized by BPL Governing Council. The BPL is one of the three professional cricket leagues in Bangladesh. It is the 16th most attended premier league in the world. In winter, each team faces the other twice in the league stage.",
+    },
+    {
+        "id": "a5",
+        "imageToUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkNB-VOp3ltBzppnpETi4I8fonx1bzkcU2_g&usqp=CAU",
+        "teamName": "Srilanka Premium League (LPL)",
+        "description": "The Lanka Premier League is a professional franchise cricket league established in 2020 in Sri Lanka. Matches are played using the Twenty20 cricket format by five teams named after Sri Lankan cities. The league was intended to commence in 2018, but repeatedly postponed by Sri Lanka Cricket",
+    },
+]
+export default WorldLeagues;
