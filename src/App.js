4597d8ae1d8da77634796e16addc92c1ad9af827
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-24 04:14:23 -0700

    remove hom page and add cricket page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import './App.css';
-import Navbar from './Component/Home/Header/Navbar';
-import LoginForm from './Component/Home/LoginForm/LoginForm';
-import SignUpForm from './Component/Home/SignUpForm/SignUpForm';
+// import Navbar from './Component/Home/Header/Navbar';
+// import LoginForm from './Component/Home/LoginForm/LoginForm';
+// import SignUpForm from './Component/Home/SignUpForm/SignUpForm';
+import Cricket from './Component/Sports/Cricket/Cricket';
 
 function App() {
-  const [signUpForm, setSignUpForm] = useState(false);
-  const signUpHandler = () => {
-    setSignUpForm(true)
-  }
+  // const [signUpForm, setSignUpForm] = useState(false);
+  // const signUpHandler = () => {
+  //   setSignUpForm(true)
+  // }
   return (
     <Fragment>
-      <Navbar signUp={signUpHandler} />
-      {!signUpForm ? <LoginForm /> : <SignUpForm />}
+      {/* <Navbar signUp={signUpHandler} />
+      {!signUpForm ? <LoginForm /> : <SignUpForm />} */}
+      <Cricket />
     </Fragment>
   );
 }
