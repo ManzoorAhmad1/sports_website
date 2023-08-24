c58e189dd08cf6238e4d8a28357b6ef8eee66e16
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-24 04:16:19 -0700

    fetch data for cricket sports

---
+++ src/Component/Sports/Cricket/CricketHome.js
@@ -0,0 +1,27 @@
+import Cricket from "../../../Backened_Folder/Sports/Cricket";
+import classes from "./Cricket.module.css"
+import Card from "../../UI/Card";
+const CricketHome = (props) => {
+    const cricket = Cricket.map((item) => {
+        return (
+            <Card key={item.id} >
+                <ul>
+                    <li><img src={item.img} alt="Error" /></li>
+                    <li><h1>{item.title}</h1></li>
+                    <li>{item.discription}</li>
+                    <li><h2>Capton: {item.Capton}</h2></li>
+                    <li>PublishedAt: {item.PublishedAt}</li>
+                </ul>
+                <div className={classes.button}>
+                    <button ><a href="https://www.icc-cricket.com/" target="_blank" rel="noreferrer">Read More</a></button>
+                </div>
+            </Card>
+        )
+    })
+    return (
+        <main >
+            {cricket}
+        </main>
+    )
+}
+export default CricketHome;
